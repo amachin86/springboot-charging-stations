ceef5df193ece0dc7ad64658a2845123c7164ed7
amachin <git-commit-address>
2024-09-30 02:07:27 -0400

    upload all

---
+++ src/main/java/com/example/demotest/DemoTestApplication.java
@@ -0,0 +1,21 @@
+package com.example.demotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+//@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
+public class DemoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTestApplication.class, args);
+    }
+
+}
